d2c7b4332cc30e82f434edc13b3d38f181d37fb8
pedrosch <pedroschroeder06@gmail.com>
2025-03-19 20:15:34 -0300

    add funcionalidades pagina de login

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("registro/", views.submit_register, name="submit_register"),
+    path("login/", views.submit_login, name="submit_login"),
+    
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,38 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def submit_register(request):
+   if request.method == 'POST':
+       nomeCompleto = request.POST.get('nomeCadastro')
+       nomeUser = request.POST.get('userCadastro')
+       email = request.POST.get('emailCadastro')
+       senha = request.POST.get('passwordCadastro')
+       
+       print(f"Nome Completo: {nomeCompleto}, Nome: {nomeUser}, Email: {email}, Senha: {senha}")
+
+       if nomeCompleto and nomeUser and email and senha:
+           message = f"Nome Completo: {nomeCompleto}, Nome: {nomeUser}, Email: {email}, Senha: {senha}"
+           return HttpResponse(message)
+       else:
+           return HttpResponse("Algum campo está vazio.")
+   else:
+       return HttpResponse("Erro ao processar.")
+   
+def submit_login(request):
+    if request.method == 'GET':
+       email = request.POST.get('loginEmail')
+       senha = request.POST.get('loginPassword')
+       
+       print(f"Email: {email}, Senha: {senha}")
+
+       if email and senha:
+           message = f"Email: {email}, Senha: {senha}"
+           return HttpResponse(message)
+       else:
+           return HttpResponse("Algum campo está vazio.")
+    else:
+       return HttpResponse("Erro ao processar.")
